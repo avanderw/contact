bafaa0533746f6c183f323593314a7e546bb2915
Andrew van der Westhuizen <avanderw@gmail.com>
2022-11-02 13:59:57 +0200

    Add check for https

--- index.js
+++ index.js
@@ -37,8 +37,21 @@ async function getContacts() {
     }
 }
 
-if (supported) {
-    checkProperties();
+/**
+ * Warn the page must be served over HTTPS
+ * The `beforeinstallprompt` event won't fire if the page is served over HTTP.
+ * Installability requires a service worker with a fetch event handler, and
+ * if the page isn't served over HTTPS, the service worker won't load.
+ */
+if (window.location.protocol === 'http:') {
+    const requireHTTPS = document.getElementById('requireHTTPS');
+    const link = requireHTTPS.querySelector('a');
+    link.href = window.location.href.replace('http://', 'https://');
+    requireHTTPS.classList.remove("d-none");
 } else {
-    document.getElementById("supportWarning").classList.remove("d-none");
+    if (supported) {
+        checkProperties();
+    } else {
+        document.getElementById("supportWarning").classList.remove("d-none");
+    }
 }
